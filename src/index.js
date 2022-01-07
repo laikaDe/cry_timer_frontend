d7ca1fb4f588d3f0e70ef1deab5f7d6791167c66
Laika Deunert <laikadeunert@MacBook-Air.local>
2022-01-06 21:35:48 -0800

    1. Defined const timerForm that retrieve's id from form. 2. Added an addEventListener function to timerForm that triggers when the form is submitted. Takes in two arguments. "submit", and submitTimer function. 3. Created a submitTimer function that takes in the event as an argument, then adds a preventDefault function to the event to prevent the page from refreshing once submit is pressed. Then prints the result of the event being triggered(aka it's value). :wq

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!")
+const timerForm = document.getElementById("timer-form")
+
+timerForm.addEventListener("submit", submitTimer)
+
+function submitTimer(e){
+    e.preventDefault() 
+    console.log(e.target.children[0].value)
+    const li = document.createElement('li')
+    const p = document.createElement('p)')
+}
