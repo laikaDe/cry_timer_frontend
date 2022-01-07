e671623f52fefc5259118161cb228a8b7bd9bae7
Laika Deunert <laikadeunert@MacBook-Air.local>
2022-01-06 21:53:27 -0800

    In the submitTimer function in index.js 1. Created a const p which will create an element referred to by 'li' 2. Created a const p which will create an element referred to by 'p' in a submitted form 3. Added innerText function to p variable to print it's value whenever p is called 4. Created a const reviewForm which will add a form to the timerForm each time a new timer form submits 5. Added .innerHtml property to reviewForm in order to allow the code to manipulate the showpage 6. Added .append function to li instance sending it arguments p and function reviewForm which will allow new instance of review to be appended to a list of reviews for a timer 7. timer.List.appendChild(li)-> appends to timer List 8. timerForm.reset -> Resets timerForm

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const timerForm = document.getElementById("timer-form")
+const timerInput = document.getElementById("timer-input")
+const timerList = document.getElementById("timer-list")
 
 timerForm.addEventListener("submit", submitTimer)
 
@@ -6,5 +8,17 @@ function submitTimer(e){
     e.preventDefault() 
     console.log(e.target.children[0].value)
     const li = document.createElement('li')
-    const p = document.createElement('p)')
+
+    const p = document.createElement('p')
+    p.innerText = timerInput.value
+
+    const reviewForm = document.createElement('form')
+    reviewForm.innerHTML += `<input type="text"><input
+    type="submit">`
+
+    li.append(p, reviewForm)
+
+    timerList.appendChild(li)
+
+    timerForm.reset()
 }
